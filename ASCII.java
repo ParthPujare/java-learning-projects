9516d38b270307fdd2f95fa5ee8ad82e9a47d8ca
ParthPujare <76155072+ParthPujare@users.noreply.github.com>
2024-02-04 10:21:19 +0530

    Create ASCII.java
    Finding the ASCII value of the number given by the user

---
+++ ASCII.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Pg15
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		byte n = sc.nextByte();
+		System.out.println("ASCII vlaue:"+(char)n);
+	}
+}
